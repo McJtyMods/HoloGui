e83ccc351f6181e328e1a7ce03f0ad7532ff3017
McJty <mcjty1@gmail.com>
2019-01-19 08:27:03 +0100

    Version 0.0.5-beta

--- src/main/java/mcjty/hologui/HoloGui.java
+++ src/main/java/mcjty/hologui/HoloGui.java
@@ -29,7 +29,7 @@ import java.util.function.Function;
 public class HoloGui implements ModBase {
     public static final String MODID = "hologui";
     public static final String MODNAME = "HoloGui";
-    public static final String VERSION = "0.0.4-beta";
+    public static final String VERSION = "0.0.5-beta";
     public static final String MIN_FORGE11_VER = "14.23.3.2694";
     public static final String MIN_MCJTYLIB_VER = "3.1.0";
 
