cd03ac2d4395a96d55943d068bf392c0a38bcfe6
McJty <mcjty1@gmail.com>
2019-03-11 09:54:37 +0100

    More cleanups

--- src/main/java/mcjty/hologui/proxy/CommonSetup.java
+++ src/main/java/mcjty/hologui/proxy/CommonSetup.java
@@ -6,7 +6,6 @@ import mcjty.hologui.config.ConfigSetup;
 import mcjty.lib.compat.MainCompatHandler;
 import mcjty.lib.setup.DefaultCommonSetup;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -21,26 +20,20 @@ public class CommonSetup extends DefaultCommonSetup {
 
         MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers());
 
-        setupModCompat();
-
 //        HoloMessages.registerNetworkMessages("hologui");
 
         ConfigSetup.init();
         ModEntities.init();
     }
 
-    private void setupModCompat() {
+    @Override
+    protected void setupModCompat() {
         MainCompatHandler.registerWaila();
         MainCompatHandler.registerTOP();
     }
 
     @Override
     public void createTabs() {
-    }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
     }
 
     @Override
