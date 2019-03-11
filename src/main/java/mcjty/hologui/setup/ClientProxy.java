15e5d9842834ce441d748caf6899a1cbb458ef76
McJty <mcjty1@gmail.com>
2019-03-11 11:27:09 +0100

    New ClientRegistration class

--- src/main/java/mcjty/hologui/setup/ClientProxy.java
+++ src/main/java/mcjty/hologui/setup/ClientProxy.java
@@ -1,41 +1,14 @@
 package mcjty.hologui.setup;
 
-import mcjty.hologui.ModEntities;
 import mcjty.lib.McJtyLibClient;
 import mcjty.lib.setup.DefaultClientProxy;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends DefaultClientProxy {
-
-//    public static IHoloGuiRenderer holoGuiRenderer = new HoloGuiRenderer();
 
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         super.preInit(e);
-        MinecraftForge.EVENT_BUS.register(this);
-//        MinecraftForge.EVENT_BUS.register(new ClientForgeEventHandlers());
         McJtyLibClient.preInit(e);
-    }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-//        MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
-//        KeyBindings.init();
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-    }
-
-    @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-        ModEntities.initModels();
     }
 }
---
+++ src/main/java/mcjty/hologui/setup/ClientRegistration.java
@@ -0,0 +1,18 @@
+package mcjty.hologui.setup;
+
+
+import mcjty.hologui.ModEntities;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.CLIENT)
+public class ClientRegistration {
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        ModEntities.initModels();
+    }
+
+}
