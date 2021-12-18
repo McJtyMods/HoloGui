722ae6610679dcc8b8cd8bd1cde2a94f6c78df85
Ray Rehbein <mrrehbein@gmail.com>
2021-12-17 18:31:52 -0600

    Update entity renderer registration

--- src/main/java/mcjty/hologui/setup/ClientSetup.java
+++ src/main/java/mcjty/hologui/setup/ClientSetup.java
@@ -1,19 +1,17 @@
 package mcjty.hologui.setup;
 
-
 import mcjty.hologui.ModEntities;
 import mcjty.hologui.gui.HoloGuiEntityRender;
 import mcjty.hologui.gui.HoloGuiSpriteUploader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
-// @todo 1.18 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 
 public class ClientSetup {
 
-    public static void init(FMLClientSetupEvent event) {
-        // @todo 1.18 RenderingRegistry.registerEntityRenderingHandler(ModEntities.HOLOGUI_ENTITY_TYPE.get(), HoloGuiEntityRender::new);
+    public static void entityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.HOLOGUI_ENTITY_TYPE.get(), HoloGuiEntityRender::new);
     }
 
     public static void setupSpriteUploader() {
